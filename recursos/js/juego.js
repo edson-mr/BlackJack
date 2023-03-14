29878b51df432ad42ad9cb773947c5aee6b68df4
edson mendoza <95mendozaaldair@gmail.com>
2023-03-14 09:19:16 -0500

    agregando funcion pedirCarta

--- recursos/js/juego.js
+++ recursos/js/juego.js
@@ -1,20 +1,33 @@
 
-
 const tipos = ['C','D','H','S'];
 const especiales=['A','J','Q','K'];
 let cartas=[];
 
-for (let i = 2; i <=10; i++) {
-   for (const tipo of tipos) {
-        cartas.push(i+tipo);
-   } 
-}
-for (const especial of especiales) {
-    for (const tipo of tipos) {
-        cartas.push(especial+tipo);
+const btnNuevo= document.querySelector(".btnNuevo");
+const btnPedir = document.querySelector(".btnPedir");
+const btnDetener = document.querySelector(".btnDetener");
+
+const crearBaraja =()=>{
+
+    for (let i = 2; i <= 10; i++) {
+        for (const tipo of tipos) {
+            cartas.push(i + tipo);
+        }
     }
+
+    for (const especial of especiales) {
+        for (const tipo of tipos) {
+            cartas.push(especial + tipo);
+        }
+    }
+
+    cartas = _.shuffle(cartas);
+
+    console.log(cartas);
+
 }
 
-cartas = _.shuffle(cartas);
+const pedirCarta=()=>cartas.pop();
 
-console.log(cartas);
+crearBaraja();
+console.log(pedirCarta());
