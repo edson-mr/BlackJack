a0aeb9dcf0216a907094dc845572dc6dcab7fcfa
edson mendoza <95mendozaaldair@gmail.com>
2023-03-14 10:01:13 -0500

    agregando el evento pedirCarta

--- recursos/js/juego.js
+++ recursos/js/juego.js
@@ -1,11 +1,15 @@
 const tipos = ["C", "D", "H", "S"];
 const especiales = ["A", "J", "Q", "K"];
 let cartas = [];
+let puntosJugador=0, puntosComputadora=0;
 
 const btnNuevo = document.querySelector(".btnNuevo");
 const btnPedir = document.querySelector(".btnPedir");
 const btnDetener = document.querySelector(".btnDetener");
-
+const jugadorCartas = document.getElementById("jugador-cartas");
+const computadoraCartas= document.getElementById("computadora-cartas");
+const puntoJugador= document.querySelector(".punto-jugador");
+const puntoComputadora= document.querySelector(".punto-computadora");
 const crearBaraja = () => {
   for (let i = 2; i <= 10; i++) {
     for (const tipo of tipos) {
@@ -27,10 +31,42 @@ const crearBaraja = () => {
 const pedirCarta = () => cartas.pop();
 const valorCarta = (carta) => {
   const valor = carta.substring(0, carta.length - 1);
-  const val = isNaN(valor) ? (valor == "A" ? 11 : 10) : valor * 1;
-  console.log(val);
+  return isNaN(valor) ? (valor == "A" ? 11 : 10) : valor * 1;
 };
 
+const turnoComputadora=(puntosJugador)=>{
+    do {
+
+       const carta = pedirCarta();
+       const valor = valorCarta(carta);
+       puntosComputadora += valor;
+       puntoComputadora.textContent = puntosComputadora;
+      const imgCarta = document.createElement("img");
+      imgCarta.classList.add("carta");
+      imgCarta.src = `recursos/cartas/${carta}.png`;
+      computadoraCartas.appendChild(imgCarta);
+
+    } while (puntosJugador < 21);
+}
+
 crearBaraja();
-console.log(pedirCarta());
-valorCarta("3C");
+
+btnPedir.addEventListener("click", () => {
+  const carta = pedirCarta();
+  const valor = valorCarta(carta);
+  puntosJugador+=valor;
+  puntoJugador.textContent=puntosJugador;
+
+  const imgCarta = document.createElement("img");
+  imgCarta.classList.add("carta");
+  imgCarta.src = `recursos/cartas/${carta}.png`;
+  jugadorCartas.appendChild(imgCarta);
+
+  if(puntosJugador >= 21){
+    turnoComputadora(puntosJugador);
+  }
+
+});
+btnDetener.addEventListener("click",()=>{
+    turnoComputadora(puntosJugador);
+});
